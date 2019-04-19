f85fbdd45c8e8a81e757f399dda771f9b1fa4030
beaker113 <43103384+beaker113@users.noreply.github.com>
2019-04-18 20:19:01 -0700

    added over complicated venting mechanic

---
+++ deucebags/src/components/apicomps/showform.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Journal from './journal'
+
+
+
+class Showform extends Component { 
+    constructor() {
+super()
+    
+    this.state = { showResults: false }
+
+    this.handleClick = this.handleClick.bind(this) 
+  };
+
+
+    
+    handleClick() {
+            this.setState({ showResults: true })          
+    }    
+
+    render() {
+            return (
+                <div>
+                    <button value="Search" onClick={this.handleClick}>Journal</button>
+                    { this.state.showResults ? <Journal /> : null }
+                </div>
+            );
+        }
+    }
+
+    // var Results = React.createClass({
+    //     render: function() {
+    //         return (
+    //             <div id="results" className="search-results">
+    //                 <Journal />
+    //             </div>
+    //         );
+    //     }
+    // });
+    
+  export default Showform;
