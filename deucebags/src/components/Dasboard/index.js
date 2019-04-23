158900579deaebd3b79f274c343dbc378e8053de
Hatshepsut <amabelle.delacruz322@gmail.com>
2019-04-23 14:50:03 -0700

    fixed modal

--- deucebags/src/components/Dasboard/index.js
+++ deucebags/src/components/Dasboard/index.js
@@ -1,59 +1,155 @@
 import React from 'react'
-import {firebase} from '../../base'
-import unirest from 'unirest'
-import axios from 'axios'
+import { databaseBase, firebase } from '../../base'
+import Dashboard from './components/Dasboard.js';
 
-export default class Dashboard extends React.Component{
-  constructor(props){
+export default class Login extends React.Component {
+  constructor() {
     super()
     this.state = {
-
+      authenticated: false,
+      items: []
     }
   }
 
-  handleAddItemChange = (event) => {
-    this.setState({newItem: event.target.value});
+  handleCreateUserEmailChange = (event) => {
+    this.setState({ createUserEmail: event.target.value });
   }
 
-  addItem = (event) => {
+  handleCreateUserPasswordChange = (event) => {
+    this.setState({ createUserPassword: event.target.value });
+  }
+
+  handleLoginEmailChange = (event) => {
+    this.setState({ signInEmail: event.target.value });
+  }
+
+  handleLoginPasswordChange = (event) => {
+    this.setState({ signInPassword: event.target.value });
+  }
+
+  createUser = (event) => {
     event.preventDefault()
-    firebase.database().ref('/to-do-list').push({
-      item: this.state.newItem,
-      uid: this.props.uid
+    const promise = firebase.auth().createUserWithEmailAndPassword(this.state.createUserEmail, this.state.createUserPassword)
+    // promise.catch(e => console.log(e.message))
+    promise.then(() => {
+      firebase.auth().currentUser.updateProfile({
+        displayName: this.state.displayName
+      })
     })
-    document.getElementById("add-item-form").reset();
+
+    promise.catch(e => {
+      console.log(e.message)
+      this.setState({
+        error: e.message
+      })
+      setTimeout(() => {
+        this.setState({
+          error: ""
+        })
+      }, 3000)
+    })
   }
 
-
-  deleteItem = (key) => {
-    firebase.database().ref('/to-do-list').child(key).remove();
+  signIn = (event) => {
+    event.preventDefault()
+    const promise = firebase.auth().signInWithEmailAndPassword(this.state.signInEmail, this.state.signInPassword)
+    // promise.catch(e => console.log(e.message))
+    promise.catch(e => {
+      console.log(e.message)
+      this.setState({
+        error: e.message
+      })
+      setTimeout(() => {
+        this.setState({
+          error: ""
+        })
+      }, 3000)
+    })
   }
 
-  render(){
+  signOut = () => {
+    firebase.auth().signOut()
+  }
 
-    const listItems = this.props.items.map((eachItem, key) =>
-      <p key={eachItem.key}>{eachItem.item} <button onClick={() => this.deleteItem(eachItem.key)}>x</button></p>
-    );
+  componentDidMount() {
+    console.log('hello');
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+      if (firebaseUser) {
+        console.log(firebaseUser);
+        this.setState({
+          authenticated: true,
+          uid: firebaseUser.uid
+        })
+        databaseBase.syncState('/to-do-list', {
+          context: this,
+          state: 'items',
+          asArray: true,
+          keepKeys: true,
+          queries: {
+            orderByChild: 'uid',
+            equalTo: this.state.uid
+          }
+        });
+      } else {
+        console.log('not logged in');
+        this.setState({ authenticated: false })
+      }
+    })
+  }
 
+  render() {
     return (
-      <div>
-        {/* Authorization  */}
-       <div id="to-do-list" className="dashed-container">
-         <div className="cut-sentence">
-           <i className="fa fa-scissors"></i> cut and use in your program
-           <span className="orange-words">Authorization</span>
-         </div>
+      <div className="App">
+        {/* Header   */}
 
-         <form id="add-item-form" onSubmit={this.addItem}>
-           <h3>To do list</h3>
-           <input value={this.state.value} onChange={this.handleAddItemChange} type="text" placeholder="to do item" required></input>
-           <button type="submit">Add item</button>
-         </form>
+        {/* Authentication  */}
+        <div className="dashed-container">
 
-         <div id="to-do-list-items">{listItems}</div>
+          <div className="cut-sentence">
+            <i className="fa fa-scissors"></i> cut and use in your program
+        <span className="orange-words">Authentication</span>
+          </div>
 
-       </div>
+          {this.state.authenticated === false &&
+            <div>
+              <form id="create-user-form" onSubmit={this.createUser}>
+                <h2>Create user</h2>
+                <input value={this.state.value} onChange={this.handleCreateUserEmailChange} type="email" placeholder="Email" required></input>
+                <input value={this.state.value} onChange={this.handleCreateUserPasswordChange} type="password" placeholder="Password" required></input>
+                <button id="sign-up-button" type="submit">Sign Up</button>
+              </form>
+
+              <form id="sign-in-form" onSubmit={this.signIn}>
+                <h2>Sign in</h2>
+                <input value={this.state.value} onChange={this.handleLoginEmailChange} type="email" placeholder="Email" required></input>
+                <input value={this.state.value} onChange={this.handleLoginPasswordChange} type="password" placeholder="Password" required></input>
+                <button id="signIn-button" type="submit">Log In</button>
+              </form>
+
+              <p id="errors">{this.state.error}</p>
+
+            </div>
+          }
+          {this.state.authenticated === true &&
+            <button id="sig-out-button" onClick={this.signOut}>Log Out</button>
+          }
+        </div>
+
+        {/* Errors  */}
+        {
+          (this.state.authenticated === false)
+            ? <div>status <span className="status-red">not authenticated</span></div>
+            : <div>status <span className="status-green">authenticated</span></div>
+        }
+
+        {this.state.authenticated === true &&
+          <Dashboard
+            uid={this.state.uid}
+            items={this.state.items}
+          />
+        }
+
       </div>
-    )
+    );
   }
 }
